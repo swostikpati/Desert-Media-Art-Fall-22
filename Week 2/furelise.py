5b6a90c0b10d7d0460d988952def18a48b73f6b3
Swostik Pati <swostikpati@gmail.com>
2022-09-10 12:40:24 +0400

    Update furelise.py

--- Week 2/furelise.py
+++ Week 2/furelise.py
@@ -3,9 +3,9 @@
 # SPDX-License-Identifier: MIT
 
 """CircuitPython Essentials Internal RGB LED red, green, blue example"""
-print("Start")
-import time
 import board
+import time
+print("Start")
 
 if hasattr(board, "APA102_SCK"):
     import adafruit_dotstar
@@ -18,55 +18,123 @@ else:
 
 led.brightness = 0.5
 
-'''while True:
-    for i in range(0,255,10):
-        for j in range(0,255,10):
-            for k in range(0,255,10):
-                led[0] = (i, j, k)
-                time.sleep(0.05)
-            time.sleep(0.05)
-        time.sleep(0.05)
 
-    #time.sleep(0.5)
-    #led[0] = (0, 255, 0)
-    #time.sleep(0.5)
-    #led[0] = (0, 0, 255)
-    #time.sleep(0.5)
-    break'''
-led[0] = (0, 255, 0)
-time.sleep(1)
-for i in range (4):
-    for i in range (3):
-        led[0] = (255, 0, 0)
-        time.sleep(0.2)
-        led[0] = (0, 0, 255)
-        time.sleep(0.2)
-
+def red():
     led[0] = (255, 0, 0)
-    time.sleep(0.2)
+
+
+def green():
     led[0] = (0, 255, 0)
-    time.sleep(0.2)
+
+
+def blue():
     led[0] = (0, 0, 255)
+
+
+def yellow():
+    led[0] = (255, 255, 0)
+
+
+def magenta():
+    led[0] = (255, 0, 255)
+
+
+def aqua():
+    led[0] = (0, 255, 255)
+
+
+def black():
+    led[0] = (0, 0, 0)
+
+
+def white():
+    led[0] = (255, 255, 255)
+
+
+def part1():
+    for i in range(3):
+        red()
+        time.sleep(0.2)
+        blue()
+        time.sleep(0.2)
+
+    #led[0] = (255, 0, 0)
+    red()
     time.sleep(0.2)
-    led[0] = (0, 255, 0)
+    #led[0] = (0, 255, 0)
+    green()
+    time.sleep(0.2)
+    #led[0] = (0, 0, 255)
+    blue()
+    time.sleep(0.2)
+    #led[0] = (0, 255, 0)
+    green()
     time.sleep(0.2)
 
     for i in range(5, 0, -1):
         led.brightness = i/10
-        led[0] = (255, 0, 0)
-        
-        time.sleep(0.3)
-        
-    for i in range(5, 0, -1):
-        led.brightness = i/10
-        led[0] = (0, 0, 255)
-        
+        #led[0] = (255, 0, 0)
+        red()
+
         time.sleep(0.3)
 
     for i in range(5, 0, -1):
         led.brightness = i/10
-        led[0] = (0, 255, 0)
-        
+        #led[0] = (0, 0, 255)
+        blue()
+
         time.sleep(0.3)
 
+    for i in range(5, 0, -1):
+        led.brightness = i/10
+        #led[0] = (0, 255, 0)
+        green()
+
+        time.sleep(0.3)
+
+
+def part2():
+    for i in range(1, 6):
+        led.brightness = i/10
+        aqua()
+        time.sleep(0.3)
+
+    for i in range(5, 0, -1):
+        led.brightness = i/10
+        yellow()
+        time.sleep(0.3)
+
+    for i in range(5, 0, -1):
+        led.brightness = i/10
+        magenta()
+        time.sleep(0.3)
+    for i in range(5, 0, -1):
+        led.brightness = i/10
+        aqua()
+        time.sleep(0.3)
+
+    for i in range(6):
+        magenta()
+        time.sleep(0.25)
+        yellow()
+        time.sleep(0.25)
+
+
+white()
+time.sleep(1)
+for i in range(4):
+    part1()
+
+part2()
+
+for i in range(2):
+    part1()
+
+part2()
+
+for i in range(2):
+    part1()
+
+white()
+time.sleep(1)
 print("Done")
